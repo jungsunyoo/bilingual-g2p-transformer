a8935d337e70605beeeec048d19bcd8662d08311
yoojungsun0 <yoojungsun0@gmail.com>
2019-11-28 22:11:02 +0900

    5

--- hparams.py
+++ hparams.py
@@ -3,49 +3,34 @@ import argparse
 class Hparams:
     parser = argparse.ArgumentParser()
 
-    # prepro
-#    parser.add_argument('--vocab_size', default=100000000, type=int)
-    parser.add_argument('--vocab_size', default=32000, type=int)
-#    parser.add_argument('--vocab_size', default=24, type=int)
     # train
     ## files
     parser.add_argument('--train1', default='sentences/train.g.reduced.bpe',
-                             help="english grapheme training data")
+                             help="english & korean grapheme training data")
     parser.add_argument('--train2', default='sentences/train.p.reduced.bpe',
-                             help="english phoneme training data")
+                             help="english & korean phoneme training data")
     parser.add_argument('--eval1', default='sentences/eval.g.bpe',
-                             help="english grapheme evaluation data")
+                             help="english & korean grapheme evaluation data")
     parser.add_argument('--eval2', default='sentences/eval.p.bpe',
-                             help="english phoneme evaluation data")
-#    parser.add_argument('--eval3', default='iwslt2016/prepro/eval.de',
-#                             help="english evaluation unsegmented data")
-    ## YJS ADDED: MULTI-LANGUAGE
-#    parser.add_argument('--language', default='english', help="which language (eng/kor, def=eng)")
+                             help="english & korean phoneme evaluation data")
     ## vocabulary
     parser.add_argument('--vocab', default='iwslt2016/segmented/bpe.vocab',
                         help="vocabulary file path")
 
     # training scheme
-    # parser.add_argument('--batch_size', default=1, type=int)
-    # parser.add_argument('--eval_batch_size', default=1, type=int)
     parser.add_argument('--batch_size', default=15, type=int)
-#    parser.add_argument('--batch_size', default=128, type=int)
     parser.add_argument('--eval_batch_size', default=15, type=int)
 
     parser.add_argument('--lr', default=0.0003, type=float, help="learning rate")
     parser.add_argument('--warmup_steps', default=4000, type=int)
-    # parser.add_argument('--warmup_steps', default=1, type=int)
     parser.add_argument('--logdir', default="log/1", help="log directory")
     parser.add_argument('--num_epochs', default=1000, type=int)
-#    parser.add_argument('--num_epochs', default=1000, type=int)
     parser.add_argument('--evaldir', default="eval/1", help="evaluation dir")
 
     # model
     parser.add_argument('--d_model', default=128, type=int,
                         help="hidden dimension of encoder/decoder")
     
-#    parser.add_argument('--d_model', default=512, type=int,
-#                        help="hidden dimension of encoder/decoder")
     parser.add_argument('--d_ff', default=2048, type=int,
                         help="hidden dimension of feedforward layer")
     parser.add_argument('--num_blocks', default=6, type=int,
@@ -63,8 +48,6 @@ class Hparams:
     # test
     parser.add_argument('--test1', default='sentences/test.short.g.kor2.bpe',
                         help="german test segmented data")
-    # parser.add_argument('--test2', default='iwslt2016/prepro/test.en',
-    #                     help="english test data")
     parser.add_argument('--ckpt', default="log/1",help="checkpoint file path")
     parser.add_argument('--test_batch_size', default=15, type=int)
     parser.add_argument('--testdir', default="test/1", help="test result dir")
